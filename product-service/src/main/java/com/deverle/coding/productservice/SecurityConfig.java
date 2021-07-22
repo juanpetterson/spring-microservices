d539b19eac2e2aae1aef1603380ff12f5a32477a
Juan Petterson Heberle <juan.petterson@gmail.com>
2021-07-21 22:48:42 -0300

    add api gateway service

---
+++ product-service/src/main/java/com/deverle/coding/productservice/SecurityConfig.java
@@ -0,0 +1,18 @@
+package com.deverle.coding.productservice;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(HttpSecurity security) throws Exception {
+        security.authorizeRequests(authorize -> authorize.anyRequest().authenticated())
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+    }
+}
