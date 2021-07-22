f61266c0d44ac662f91d442a60081d89f98683f7
Juan Petterson Heberle <juan.petterson@gmail.com>
2021-07-21 22:49:07 -0300

    configure keycloak on product service

--- product-service/src/main/java/com/deverle/coding/productservice/SecurityConfig.java
+++ product-service/src/main/java/com/deverle/coding/productservice/SecurityConfig.java
@@ -2,12 +2,12 @@ package com.deverle.coding.productservice;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
-import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 
 @Configuration
-@EnableWebFluxSecurity
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
